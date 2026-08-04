d0d5cbd6b6600759c1d885e2fa73153bdf1466dd
David Blandón <djesus1906@protonmail.com>
2026-08-05 00:46:26 +0200

    Add: pascals-triangle - submission-0

---
+++ Data Structures & Algorithms/pascals-triangle/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        dp = [[0] * x for x in range(1, numRows + 1)]
+
+        for i in range(numRows):
+            dp[i][0] = 1
+            dp[i][i] = 1
+
+            for j in range(1, i):
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+
+        return dp
